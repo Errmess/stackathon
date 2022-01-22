03fd307f7978c77a68a3adfa0268da3770046995
Seldon Tselung <stselung@gmail.com>
2022-01-22 16:01:43 -0500

    file management

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom"
+import HomepageStore from "./Homepage"
+import "./style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <Provider store={HomepageStore}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("app")
 );
 
 // If you want to start measuring performance in your app, pass a function
