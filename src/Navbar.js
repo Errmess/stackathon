1f36049b03a699015eb94608c71b30804e1c066c
Tenzin Choetso <tenzinchoetso93@gmail.com>
2022-01-24 12:37:12 -0500

    routePath fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,32 +2,27 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
+  render() {
+    const linkStyle = {
+      color: "Black",
+    };
 
-    render() {
-      const linkStyle = {
-        color: "Black"
-      }
-
-      const guestLinks = (
-        <div>
+    return (
+      <nav>
+        <div className="nav-bar">
           <Link to="/" style={linkStyle}>
-           Home
+            Home
           </Link>
           <Link to="/aboutUs" style={linkStyle}>
             About Us
           </Link>
-          <Link to="/bedtimestories" style={linkStyle}>
+          <Link to="/bedtimeStories" style={linkStyle}>
             Bedtime Stories
           </Link>
         </div>
-      );
-
-    return (
-      <nav>
-        {guestLinks}
       </nav>
     );
-    }
-};
+  }
+}
 
 export default Navbar;
---
+++ src/RoutePath.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+import HomePage from "./HomePage";
+import AboutUs from "./AboutUs";
+import BedtimeStories from "./BedtimeStories";
+
+class RoutePath extends Component {
+  render() {
+    return (
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/bedtimeStories" element={<BedtimeStories />} />
+        </Routes>
+      </div>
+    );
+  }
+}
+
+export default RoutePath;
