bc1c339c69beeea3544edb486b2b477f0830801a
Tenzin Choetso <tenzinchoetso93@gmail.com>
2022-01-24 22:09:37 -0500

    fixed latest changes

--- src/BedtimeStories.js
+++ src/BedtimeStories.js
@@ -5,7 +5,7 @@ export default class BedtimeStories extends React.Component {
   constructor(props) {
     super(props);
     const storyOne =
-      "Once upon a time a girl named Cinderella lived with her stepmother and two stepsisters. Poor Cinderella had to work hard all day long so the others could rest. It was she who had to wake up each morning when it was still dark and cold to start the fire. It was she who cooked the meals. It was she who kept the fire going. The poor girl could not stay clean, from all the ashes and cinders by the fire.";
+      "Once upon a time a girl named Cinderella lived with her stepmother and two stepsisters. Poor Cinderella had to work hard all day long so the others could rest. It was she who had to wake up each morning when it was still dark and cold to start the fire. It was she who cooked the meals. It was she who kept the fire going. The poor girl could not stay clean, from all the ashes and cinders by the fire..";
     this.stories = [storyOne];
     translateText(storyOne);
   }
